e3c2119eae7b131b0b536eb799765a251875550d
d3vd00m <34721259+d3vd00m@users.noreply.github.com>
2022-05-30 17:44:21 +0200

    .

--- app/pages/api/[tokenId].js
+++ app/pages/api/[tokenId].js
@@ -2,7 +2,7 @@ export default function handler(req, res) {
     // get the tokenId from the query params
     const tokenId = req.query.tokenId;
     // All the images are uploaded on github so we can extract the images from github directly.
-    const image_url = "https://github.com/d3vd00m/NFT-Collection/tree/main/app/public/cryptodevs/";
+    const image_url = "https://raw.githubusercontent.com/d3vd00m/NFT-Collection/main/app/public/cryptodevs/";
     // The api is sending back metadata for a Crypto Dev
     // To make our collection compatible with Opensea, we need to follow some Metadata standards
     // when sending back the response from the api
